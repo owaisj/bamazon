cd85a1cd8fb89403723113986c87900f5b4a0d5b
owaisj <jamil.owais@gmail.com>
2019-05-19 23:05:11 -0500

    10 items to populate products table

---
+++ seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES
+    ('The Stand', 'Books', 15.00, 25),
+    ('Vision', 'Books', 20.00, 20),
+    ('The Wind Up Bird Chronicle', 'Books', 10.00, 20),
+    ('Crime and Punishment', 'Books', 7.50, 25),
+    ('Creatine', 'Health', 10.00, 50),
+    ('Turmeric', 'Health' 12.50, 30),
+    ('Moleskine Notebook', 'Stationary', 10.00, 75),
+    ('G2 Pens', 'Stationary', 3.00, 100),
+    ('Oreos', 'Snacks', 3.00, 150),
+    ('Goldfish','Snacks', 2.50, 200);
