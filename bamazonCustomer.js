2678724f8d845357ccf91167f943df16cd8fa7b9
owaisj <jamil.owais@gmail.com>
2019-05-20 11:18:59 -0500

    checkout function for user input and table update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const inquirer = require('inquirer');
 const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -8,11 +9,55 @@ const connection = mysql.createConnection({
     database: 'bamazon'
 });
 
+function checkout() {
+    inquirer.prompt([
+        {
+            type: 'number',
+            name: 'id',
+            message: 'Please enter the id of the item you\'d like to buy.'
+        },
+        {
+            type: 'number',
+            name: 'quantity',
+            message: 'How many would you like?'
+        }
+    ]).then(function(response){
+        let userQuantity = response.quantity;
+        let userChoice = response.id;
+        console.log('You want', userQuantity, 'of item', userChoice);
+        connection.query(
+            `SELECT * FROM products WHERE item_id=${userChoice}`,
+            function(error, response) {
+                if (error) throw error;
+                //console.table(response[0]['stock_quantity']);
+                //TODO: Check if there is enough in stock
+                let newQuantity = response[0]['stock_quantity'] - userQuantity;
+                connection.query(
+                    `UPDATE products SET ? WHERE ?`,
+                    [
+                        {
+                            stock_quantity: newQuantity
+                        },
+                        {
+                            item_id: userChoice
+                        }
+                    ],
+                    function(error, response) {
+                        if (error) throw error;
+                        console.log('Thanks for your purchase!');
+                        connection.end();
+                    }
+                )
+            }
+        )
+    }).catch();
+}
+
 connection.query(
     'SELECT * FROM products',
     function(error, response){
         if (error) throw error;
         console.table(response);
-        connection.end();
+        checkout();
     }
 )
