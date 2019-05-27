f5984cc9ff7e89de49c32909be506889c4960f55
owaisj <jamil.owais@gmail.com>
2019-05-27 11:51:59 -0500

    Inquirer promise with three options

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -9,21 +9,47 @@ const connection = mysql.createConnection({
     database: 'bamazon'
 });
 
-// (function supervise(){
-//     inquirer.prompt().then().catch();
+//TODO: Inquire user on what action they'd like to conduct
+(function supervise(){
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'operation',
+            message: 'What would you like to do?',
+            choices: [
+                'Create a new department',
+                'View total sales', //TODO: By department
+                'Exit'
+            ]
+        }
+    ]).then(r => {
+        switch (r.operation) {
+            case 'Create a new department':
+                return console.log('A new inquiry will go here.');
+            case 'View total sales':
+                return viewSales();
+            default: console.log('Thank you for using the Bamazon Supervising Suite');
+        }
+    }).catch();
 
-// })();
+})();
 
-function newDepartment() {
+//Create new department x with overhead costs y
+function newDepartment(x, y) {
+    //TODO: New inquirer prompt, return query as part of promise
+    inquirer.prompt().then().catch();
+    
     connection.query(
         `INSERT INTO departments (department_name, over_head_costs)
-        VALUES ('Books', 100)`,
+        VALUES ('${x}', ${y})`,
         function (error, response) {
             if (error) throw error;
+            console.table(response);
         }
     )
 };
 
+//TODO: Allow user to choose an individual department to view.
 function viewSales() {
     connection.query(
         `SELECT departments.department_id, departments.department_name, departments.over_head_costs,
@@ -40,5 +66,3 @@ function viewSales() {
         }
     );
 };
-
-viewSales();
